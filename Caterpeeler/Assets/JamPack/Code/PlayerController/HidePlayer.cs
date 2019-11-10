93e96fd79fc2f60623f4a6915bab1338032284ef
Ceilidh Simon <43501977+LeeSimon77@users.noreply.github.com>
2019-11-09 19:09:29 -0600

    Bird and Camera follow

---
+++ Caterpeeler/Assets/JamPack/Code/PlayerController/HidePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidePlayer : MonoBehaviour
+{
+
+    public bool hidden;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hidden = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Hideable")
+        {
+            hidden = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Hideable")
+        {
+            hidden = false;
+        }
+    }
+}
