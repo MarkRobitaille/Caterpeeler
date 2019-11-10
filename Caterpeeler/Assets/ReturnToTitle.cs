370d5786b55325b70d7489d4642e3b0359db9db1
Mark Robitaille <mark.rj.robitaille@gmail.com>
2019-11-10 12:40:52 -0600

    Created Win and Game Over Screens

---
+++ Caterpeeler/Assets/ReturnToTitle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToTitle : MonoBehaviour
+{
+
+    public float displayTime = 4; // ms?
+    public string titleScreenScene = "TitleScreen";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("CREATING WIN SCREEN");
+        StartCoroutine(LoadTitleScreen(displayTime));
+
+    }
+
+    IEnumerator LoadTitleScreen(float displayTime)
+    {
+        Debug.Log("I AM LOADING THE TITLE SCREEN");
+        yield return new WaitForSeconds(displayTime);
+        SceneManager.LoadScene(titleScreenScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
