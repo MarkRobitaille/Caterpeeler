b133b1d630f39946ebccc946da7253e483f56c65
Ceilidh Simon <43501977+LeeSimon77@users.noreply.github.com>
2019-11-10 11:07:58 -0600

    Title Screen Prefab created and completed

---
+++ Caterpeeler/Assets/closeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closeScript : MonoBehaviour
+{
+    public UnityEngine.UI.Button button;
+    public GameObject objectToDeactivate;
+
+    void Start()
+    {
+        button.onClick.AddListener(delegate { DeactiveObject(objectToDeactivate); });
+    }
+
+    void DeactiveObject(GameObject go)
+    {
+       objectToDeactivate.SetActive(false);
+    }
+}
